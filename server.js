91f7077b9520831b3e050170554f12608ef68f74
James Jarvis <jamespjarvis@gmail.com>
2017-02-22 18:10:38 -0500

    minified

--- server.js
+++ server.js
@@ -4,33 +4,43 @@ const fs = require('fs');
 let server = http.Server(requestHandler);
 
 function requestHandler(req, res) {
-	if(req.url === '/') {
-		fs.readFile('dist/index.html', (err, data) => {
-			if(err) throw err;
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.write(data);
-			res.end();	
-		});
-	} else if(req.url === '/bundle.js') {
-		fs.readFile('dist/bundle.js', (err, data) => {
-			if(err) throw err;
-			res.writeHead(200, {'Content-Type': 'application/javascript' });
-			res.write(data);
-			res.end();
-		});
-	} else if(req.url === '/style.css') {
-		fs.readFile('dist/style.css', (err, data) => {
-			if(err) throw err;
-			res.writeHead(200, { 'Content-Type': 'text/css' });
-			res.write(data);
-			res.end();
-		});
-	} else {
-		res.writeHead(302, { 'Location': '/' });
-		res.end();
-	}
+    if (req.url === '/') {
+        fs.readFile('dist/index.html', (err, data) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    } else {
+        let mimeType;
+        let filePath = 'dist' + req.url;
+        let ext = filePath.split('.');
+        switch (ext[ext.length - 1]) {
+            case 'js':
+                mimeType = 'application/javascript';
+                break;
+            case 'css':
+                mimeType = 'text/css';
+                break;
+            default:
+                mimeType = 'text/plain';
+                break;
+        }
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                if (err.code === 'ENOENT') {
+                    console.error(`${filePath} does not exist`);
+                    return;
+                } else {
+                    throw err;
+                }
+            } else {
+                res.writeHead(200, { 'Content-Type': mimeType });
+                res.write(data);
+                res.end();
+            }
+        });
+    }
 };
 
 server.listen(8080, () => console.log('server running'));
-
-	
