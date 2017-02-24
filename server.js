b4ab8cae2a136e23b06a741bf672440bcb836cc4
James Jarvis <jamespjarvis@gmail.com>
2017-02-24 09:29:51 -0500

    added fs.stat to get download size

--- server.js
+++ server.js
@@ -2,47 +2,53 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
-let server = http.Server(requestHandler);
-
-function requestHandler(req, res) {
-    let filePath = 'dist' + req.url;
-    if (req.url === '/') filePath = 'dist/index.html';
-    let extName = path.extname(filePath);
-    console.log(extName);
-    let resHeader;
-    switch (extName) {
-        case '.html':
-            resHeader = { 'Content-Type': 'text/html' };
-            break;
-        case '.js':
-            resHeader = { 'Content-Type': 'application/javascript' };
-            break;
-        case '.css':
-            resHeader = { 'Content-Type': 'text/css' };
-            break;
-        case '.zip':
-            resHeader = { 'Content-Type': 'application/octet-stream' };
-            break;
-        default:
-            resHeader = { 'Location': '/' };
-            break;
-    }
-    let stream = fs.createReadStream(filePath);
-
-    stream.on('error', (err) => {
-        if (err) {
+let server = http.Server((req, res) => {
+    req.on('error', (err) => {
+        console.error(err);
+        res.statusCode = 400;
+        res.end();
+    });
+    res.on('error', (err) => console.error(err));
+    if (req.method === 'GET') {
+        let filePath = 'dist' + req.url;
+        if (req.url === '/') filePath = 'dist/index.html';
+        let readStream = fs.createReadStream(filePath);
+        readStream.on('error', (err) => {
             if (err.code === 'ENOENT') {
                 console.error(`${filePath} does not exist.`);
             } else {
                 throw err;
             }
-            res.writeHead(304, resHeader);
+            res.writeHead(304, { 'Location': '/' });
             res.end();
+        });
+        let resHeaders;
+        let extName = path.extname(filePath);
+        let stats = fs.statSync(filePath);
+        switch (extName) {
+            case '.zip':
+                resHeaders = {
+                    'Content-Type': 'application/octet-stream',
+                    'Content-Length': stats.size
+                }
+                break;
+            case '.html':
+                resHeaders = { 'Content-Type': 'text/html' };
+                break;
+            case '.js':
+                resHeaders = { 'Content-Type': 'application/javascript' };
+                break;
+            case '.css':
+                resHeaders = { 'Content-Type': 'text/css' };
+                break;
+            default:
+                resHeaders = { 'Content-Type': 'text/plain' };
+                break;
+
         }
-    });
-    
-    res.writeHead(200, resHeader)
-    stream.pipe(res);
-}
+        res.writeHead(200, resHeaders);
+        readStream.pipe(res);
+    }
+});
 
 server.listen(8080, () => console.log('Server listening at localhost:8080'));
